22e31ae932d2d8d6355ba802721c8b5f9351936d
AbeloByte <gobeztech20@gmail.com>
2025-08-22 16:33:02 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Account from "./pages/Account";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="/dashboard" />} />
+
+            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/users" element={<NewUsers />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
+
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
